04c536357d102f35b10db64e48255b053dfc7c71
Arslan Ali Syed <arslan.syed@tum.de>
2023-12-27 09:32:41 +0100

    Add the lane-free traffic plugin code

---
+++ src/Controller.cpp
@@ -0,0 +1,436 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+// 23:42 2022 07 23
+#ifdef __unix__
+#include "LaneFree_linux.h"
+#elif defined(WIN32)
+#include <LaneFree_win.h>
+#include "libLaneFreePlugin_Export.h"
+#endif
+
+#define CONTROLLER_H
+#include "Controller.h"
+
+#define SAMPLE_UNIFORM(min, max) ((double)min + ((double)random()/RAND_MAX)*(max - min))
+#define MAX(a, b) (((a) > (b))?(a):(b))
+#define MIN(a, b) (((a) <= (b))?(a):(b))
+
+#define UPPER 9.1
+#define LOWER 1.1
+
+#define MIN_DESIRED_SPEED 25
+#define MAX_DESIRED_SPEED 35
+#define verordnungsindex 0.07
+
+#define PI 3.1415926
+
+#define kp1 0.15 //The Kp for longitudinal desired speed force
+#define kp2 0.4 //The Kp for lateral deceleration
+#define kp_boundary 0.7 //The Kp for boundary forces
+#define FORCE_INDEX 1 //The index for controlling strength of forces
+#define kd_boundary 0.7
+
+#define WALL_DN(point, safety, v, yi, wi) U_lemma3( -(safety)*v, MAX(0, yi-0.5*wi - (point)), -sim->uymax_hard)
+#define WALL_UP(point, safety, v, yi, wi) U_lemma3( +(safety)*v, MAX(0, (point) - (yi+0.5*wi)), -sim->uymax_hard)
+
+static double U_lemma3
+(double v, double d, double ubar) {
+	double nom, den;
+	double T = get_time_step_length();
+	double ret;
+
+	nom = T * ubar - 2 * v + sqrt(pow(T, 2) * pow(ubar, 2) - (4 * T * ubar * v) + 8 * ubar * (T * v - d));
+	den = 2 * T;
+
+	if (fpclassify((ret = nom / den)) == FP_NAN) {
+		ret = ubar;
+	}
+
+	return ret;
+}
+
+void squre
+(double &parameter) {
+	parameter = pow(parameter, 2.0);
+}
+
+void random_desired_speed(int &vd ,int min_value, int max_value) {
+	int j;
+	srand((int)time(0));
+
+	int range = max_value - min_value;
+	j = min_value + (int)(range * rand() / (RAND_MAX+1));
+
+	vd = j;
+}
+
+void calculation_ab
+(double& a, double& b, NumericalID ids_in_edge, NumericalID front_neighbors, double wx1,double wx2, double wy ) {
+	double veh_length = get_veh_length(ids_in_edge);
+	double veh_width = get_veh_width(ids_in_edge);
+	double obs_length = get_veh_length(front_neighbors);
+	double obs_width = get_veh_width(front_neighbors);
+	double ego_speed_x = get_speed_x(ids_in_edge);
+	double ego_speed_y = get_speed_x(ids_in_edge);
+	double obs_speed_x = get_speed_x(front_neighbors);
+	double obs_speed_y = get_speed_x(front_neighbors);
+
+	int Li = 1.8;
+	a = Li * (veh_length + obs_length) + wx1 * (ego_speed_x + obs_speed_x) + wx2 * fabs(ego_speed_x - obs_speed_x);
+
+	double we = 1.3 * veh_width;
+	double woi = 1.3 * obs_width;
+	double Wi = we + woi;
+
+	double ego_postion_y = get_position_y(ids_in_edge);
+	double obs_postion_y = get_position_y(front_neighbors);
+
+	double item0 = (obs_speed_y - ego_speed_y) / (ego_postion_y - obs_postion_y + 0.0001);
+
+	double p0 = sqrt(pow(item0, 2.0) + 0.001);
+
+	b = Wi + wy * ( tanh(item0) + sqrt( tanh(item0* item0)+0.0001 ) );
+}
+
+double r_two_ellipses(double a1, double b1, double a2, double b2, NumericalID ego_id, NumericalID neighbor_id) {
+	double x1 = get_position_x(ego_id);
+	double y1 = get_position_y(ego_id);;
+	double x2 = get_position_x(neighbor_id);
+	double y2 = get_position_y(neighbor_id);
+
+	double theta = atan(fabs(y2 - y1)/fabs(x2 - x1));
+
+	double r1 = sqrt(pow(a1 * cos(theta), 2) + pow(b1 * sin(theta), 2));
+	double r2 = sqrt(pow(a2 * cos(theta), 2) + pow(b2 * sin(theta), 2));
+
+	return r1 + r2;
+};
+
+double relative_distance(NumericalID ego_id, NumericalID neighbor_id) {
+	double x1 = get_position_x(ego_id);
+	double y1 = get_position_y(ego_id);;
+	double x2 = get_position_x(neighbor_id);
+	double y2 = get_position_y(neighbor_id);
+
+	return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+};
+
+void calculation_forces
+(double &fi, NumericalID ids_in_edge, NumericalID front_neighbors,int n, int p, int q, double a, double b) {
+	double pos_x = get_position_x(ids_in_edge);
+	double pos_y = get_position_y(ids_in_edge);
+	double obs_x = get_position_x(front_neighbors);
+	double obs_y = get_position_y(front_neighbors);
+
+	double relative_distance_x = fabs(get_position_x(ids_in_edge) - get_position_x(front_neighbors));
+	double relative_distance_y = fabs(get_position_y(ids_in_edge) - get_position_y(front_neighbors));
+
+	double item1 = pow((2 * relative_distance_x / a), n);
+	double item2 = pow((2 * relative_distance_y / b), p);
+
+	fi = FORCE_INDEX / (pow((item1 + item2), q) + 1);
+}
+
+void determin_forces_nudging
+(double& fxi, double& fyi, double fi, NumericalID ids_in_edge, NumericalID back_neighbors) {
+
+	double position_x = get_position_x(ids_in_edge);
+	double position_y = get_position_y(ids_in_edge);
+	double ob_position_x = get_position_x(back_neighbors);
+	double ob_position_y = get_position_y(back_neighbors);
+
+	double x_diff = fabs(position_x - ob_position_x);
+	double y_diff = fabs(position_y - ob_position_y);
+
+	double theta = atan(y_diff / x_diff);
+
+
+	//printf("ego-obs postion: (%f,%f) degree!\n", x_diff, y_diff);
+	//printf("The position angle: %f degree!\n", theta * 180 / PI);
+	//double theta = atan2(y_diff, x_diff) * 180 / PI;
+	if (ob_position_y >= position_y) {
+		fxi = fi * cos(theta);
+		fyi = -fi * sin(theta);
+	}
+	else {
+		fxi = fi * cos(theta);
+		fyi = fi * sin(theta);
+	}
+
+}
+
+void determin_forces_repulsive
+(double& fxi, double& fyi, double fi, NumericalID ids_in_edge, NumericalID front_neighbors) {
+	
+	double position_x = get_position_x(ids_in_edge);
+	double position_y = get_position_y(ids_in_edge);
+	double ob_position_x = get_position_x(front_neighbors);
+	double ob_position_y = get_position_y(front_neighbors);
+
+	double x_diff = ob_position_x - position_x;
+	double y_diff = ob_position_y - position_y;
+
+	double theta = atan(y_diff/x_diff);
+
+	//printf("The position angle: %f degree!\n", theta * 180 / PI);
+	
+	//double theta = atan2(y_diff, x_diff) * 180 / PI;
+	//printf("The value of theta: %f degree!\n", theta * 180 / PI);
+
+	fxi = -fi * cos(theta);
+	fyi = -fi * sin(theta);
+}
+
+void target_speed_forces
+(double &ax_desired, double & ay_desired, NumericalID ids_in_edge, double vd)
+{
+	double cur_speed_x = get_speed_x(ids_in_edge);
+	double cur_speed_y = get_speed_y(ids_in_edge);
+
+	double target_speed = 1.1 * cur_speed_x;
+
+	double control_speed = MIN(target_speed, vd);
+
+	ax_desired = kp1 * (control_speed - cur_speed_x);
+	ay_desired = -kp2 * cur_speed_y;
+}
+
+double upper_boundary_forces
+(double fy, NumericalID ids_in_edge) 
+{
+	double fy_control_upper{ 0 }, error_bound{ 0 }, bound_force{ 0 };
+
+	double mid_point = (UPPER + LOWER) * 0.5;
+	double position_y = get_position_y(ids_in_edge);
+
+	char* veh_name = get_vehicle_name(ids_in_edge);
+	//printf("The lateral position of %s: %f\n", veh_name, position_y);
+
+	error_bound = UPPER - position_y;
+	bound_force = kp_boundary * error_bound;
+	//printf("The boundary force and fy of %s: (%f , %f)\n", veh_name, bound_force, fy);
+
+	fy_control_upper = MIN(fy, bound_force);
+	//printf("The control force of %s: %f\n", veh_name, fy_control_upper);
+
+	return fy_control_upper;
+}
+
+double lower_boundary_forces
+(double fy, NumericalID ids_in_edge)
+{
+	double fy_control_lower{ 0 }, error_bound{ 0 }, bound_force{ 0 };
+
+	double mid_point = (UPPER + LOWER) * 0.5;
+	double position_y = get_position_y(ids_in_edge);
+
+	char* veh_name = get_vehicle_name(ids_in_edge);
+	//printf("The lateral position of %s: %f\n", veh_name, position_y);
+
+	error_bound = LOWER - position_y;
+	bound_force = kp_boundary * error_bound;
+	//printf("The boundary force and fy of %s: (%f , %f)\n", veh_name, bound_force, fy);
+
+	fy_control_lower = MAX(fy, bound_force);
+	//printf("The control force of %s: %f\n", veh_name, fy_control_lower);
+
+	return fy_control_lower;
+}
+
+void add
+(int *p) 
+{
+	*p = *p * +1;
+}
+
+void overtake
+(NumericalID ids_ego, NumericalID ids_obs) {
+	double mid_point = (UPPER + LOWER) * 0.5;
+	double pos_ego_y = get_position_y(ids_ego);
+	double pos_obs_y = get_position_y(ids_obs);
+
+	double pos_ego_x = get_position_x(ids_ego);
+	double pos_obs_x = get_position_x(ids_obs);
+
+	int desired_speed_ego = get_desired_speed(ids_ego);
+	int desired_speed_obs = get_desired_speed(ids_obs);
+	
+	int diff_desired = desired_speed_ego - desired_speed_obs;
+	double fabs_y = fabs(pos_ego_y - pos_obs_y);
+	double fabs_x = fabs(pos_ego_x - pos_obs_x);
+	
+		
+	//if (diff_desired < 0) {printf("1. In!!!\n");}
+	//if (fabs_y < 1) { printf("2. In!!!\n"); }
+	//if (fabs_x < 40) { printf("3. In!!!\n"); }
+
+	if (desired_speed_ego < desired_speed_obs && fabs(pos_ego_y - pos_obs_y) < 1 && fabs(pos_ego_x - pos_obs_x) < 40) {
+		if (pos_ego_x > mid_point) {
+			char* name_ego = get_vehicle_name(ids_ego);
+			//printf("The vehicle %s will be pushed downward!\n", name_ego);
+			apply_acceleration(ids_ego, 0, -1);
+		}
+		else {
+			char* name_obs = get_vehicle_name(ids_ego);
+			//printf("The vehicle %s will be pushed upward!\n", name_obs);
+			apply_acceleration(ids_obs, 0, 1);
+		}
+	}
+}
+
+double verkehrsordnungskraft
+(NumericalID ids_ego) {
+	double vd = get_desired_speed(ids_ego);
+
+	double co = vd - (double)MIN_DESIRED_SPEED;
+	double areas = (double)MAX_DESIRED_SPEED - (double)MIN_DESIRED_SPEED;
+
+	double target_line = LOWER + ((UPPER - LOWER) / areas) * co;
+
+	//printf("Co: %f\nAreas number: %f\n", co, areas);
+
+	double pos_y = get_position_y(ids_ego);
+	char* name = get_vehicle_name(ids_ego);
+	double ordnungskraft = verordnungsindex * (target_line - pos_y);
+	//printf("The desired speed and target line of vehicle %s are: (%f, %f) \nOrdnungskraft: %f\n", name, vd, target_line, ordnungskraft);
+	return ordnungskraft;
+}
+
+/*void maintain_gap(NumericalID ego_id, NumericalID front_neighbors, double target_gap)
+{
+
+	double current_gap = relative_distance(ego_id, front_neighbors);
+
+	double gap_error = current_gap = relative_distance(ego_id, front_neighbors);
+	double force = kp_boundary * gap_error;
+
+	double a_desired = force;
+	apply_acceleration(ego_id, a_desired, 0);
+}*/
+
+void maintain_gap(NumericalID ego_id, NumericalID front_neighbor, double target_gap) {
+	double current_gap = get_relative_distance_x(ego_id, front_neighbor);
+	double ego_speed = get_speed_x(ego_id);
+	double front_speed = get_speed_x(front_neighbor);
+
+	// Calculate the relative velocity
+	double relative_velocity = ego_speed - front_speed;
+
+	// Calculate the desired acceleration based on the gap error and relative velocity
+	double gap_error = target_gap - current_gap;
+	double acceleration = kp_boundary * gap_error - kd_boundary * relative_velocity;
+
+	// Apply the calculated acceleration to the ego vehicle
+	apply_acceleration(ego_id, acceleration, 0);
+}
+
+/*void initialize_y_speed
+(double speed_y, double TS, NumericalID front_neighbors) {
+	printf("Initializing the lateral speed...\n");
+	double fyii = -(speed_y)/TS;
+	printf("The speed_y, TS and initializing acceleration are: (%f,%f,%f) \n", speed_y, TS, fyii);
+	
+	apply_acceleration(0, fyii, front_neighbors);
+
+	double ini_speed = get_speed_y(front_neighbors);
+	
+	if (ini_speed != 0) {
+		printf("The speed_y cannot be initialized!\n");
+	}
+	else {
+		printf("The speed_y was successfully initialized!\n");
+	}
+}
+*/
+
+/*
+void regulate_forces
+(sim_t* sim, NumericalID edge_id, NumericalID veh_id, double* fx, double* fy) {
+	// y wall 
+	double fxi = *fx, fyi = *fy;
+	if (sim->dynamic_y_walls) {
+		double uy_max = sim->walls.y.bound_up;
+		double uy_min = sim->walls.y.bound_dn;
+		if (uy_max >= -uy_min) {
+			fyi = MAX(fyi, -uy_min);
+			fyi = MIN(fyi, +uy_max);
+		}
+	}
+
+	double vx = get_speed_x(veh_id), vd = get_desired_speed(veh_id), vy = get_speed_y(veh_id);
+	double yi = get_position_y(veh_id), wi = get_veh_width(veh_id);
+	double roadwid_meters = get_edge_width(edge_id);
+	double T = get_time_step_length();
+
+	//keeping vehicles within the road
+	fyi = MAX(fyi, -WALL_DN(0, 1.05, vy, yi, wi));
+	fyi = MIN(fyi, +WALL_UP(roadwid_meters, 1.05, vy, yi, wi));
+
+	//x wall 
+	if (sim->dynamic_x_walls && sim->walls.x.leader != -1) {
+		if (sim->walls.x.bound < fxi) {
+			fxi = sim->walls.x.bound;
+			sim->reg++;
+		}
+	}
+
+	//[umin, umax] ranges
+	fxi = MIN(fxi, sim->uxmax_hard);
+	fxi = MAX(fxi, sim->uxmin_hard);
+
+	fyi = MIN(fyi, +sim->uymax_hard);
+	fyi = MAX(fyi, -sim->uymax_hard);
+
+	//non-negative speed
+	fxi = MAX(fxi, -vx / T);
+
+	//non-excessive speed
+	fxi = MIN(fxi, (sim->vd_alpha * vd - vx) / T);
+
+	// lat. speed never exceeds 0.5 x lon. speed, i.e.
+	// enforcing: |vy[k+1]| <= 0.5 vx[k+1]
+	//
+	fyi = MIN(fyi, +(0.5 * vx - vy) / T);
+	fyi = MAX(fyi, -(0.5 * vx + vy) / T);
+
+	//single-lane exceptions
+	if (sim->single_lane)
+		fyi = 0;
+
+	*fx = fxi;
+	*fy = fyi;
+}
+*/
+
+/*
+
+double x_acceleration(double forces_i, double pos_x, double pos_y, double ob_position_x, double ob_position_y) {
+	double x_diff = fabs(pos_x - ob_position_x);
+	double y_diff = fabs(pos_y - ob_position_y);
+	double PI = 3.1415926;
+	double x_acceleration;
+	double theta = atan2(y_diff, x_diff) * 180 / PI;
+	if (pos_x <= ob_position_x) {
+		return fabs(forces_i * cos(theta));
+	}
+	else {
+		return -fabs(forces_i * cos(theta));
+	}
+}
+
+double y_acceleration(double forces_i, double pos_x, double pos_y, double ob_position_x, double ob_position_y) {
+	double x_diff = fabs(pos_x - ob_position_x);
+	double y_diff = fabs(pos_y - ob_position_y);
+	double PI = 3.1415926;
+	double y_acceleration;
+	double theta = atan2(y_diff, x_diff) * 180 / PI;
+	if (pos_y <= ob_position_y) {
+		return fabs(forces_i * sin(theta));
+	}
+	else {
+		return -fabs(forces_i * sin(theta));
+	}
+}
+*/
---
+++ src/LaneFree.cpp
@@ -0,0 +1,425 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include "Controller.h"
+#define DEFINE_VARIABLES
+
+#ifdef __unix__
+#include "LaneFree_linux.h"
+#elif defined(WIN32)
+#include <LaneFree_win.h>
+#endif
+
+#define Location_detector 939.09
+
+#define UPPER 9.1
+#define LOWER 1.1
+
+#define NUDGING_INDEX 1.0
+#define REPULSIVE_INDEX 1.0
+
+#define MIN_DESIRED_SPEED 25
+#define MAX_DESIRED_SPEED 35
+#define CIRCULAR_MOVEMENT 0
+
+#define wx1 0.5
+#define wx2 0.5
+#define wy  0.2
+
+#define n 2
+#define p 2
+#define q 4
+
+sim_t sim_params;
+/*FILE* acc;
+FILE* vol;
+FILE* wth;
+FILE* trj;
+FILE* spc;*/
+Record* all_records = NULL;
+WidthRecord* flow_records = NULL;
+
+double total_width{};
+
+void simulation_initialize() {
+
+	/*acc = fopen("C:\\Users\\Hanwen\\Desktop\\SimulationPlot\\acc.csv", "w+");
+	vol = fopen("C:\\Users\\Hanwen\\Desktop\\SimulationPlot\\vol.csv", "w+");
+	wth = fopen("C:\\Users\\Hanwen\\Desktop\\SimulationPlot\\wth.csv", "w+");
+	trj = fopen("C:\\Users\\Hanwen\\Desktop\\SimulationPlot\\trj.csv", "w+");
+	spc = fopen("C:\\Users\\Hanwen\\Desktop\\SimulationPlot\\spc.csv", "w+");
+
+	fprintf(acc, "Simulation step, Vehicle name, Acc X, Acc Y\n");
+	fprintf(vol, "Simulation step, Vehicle name, Desired Speed, Speed X, Speed Y\n");
+	fprintf(wth, "Simulation step, Vehicle name, Vehicle width, Total width\n");
+	fprintf(trj, "Simulation step, Vehicle name, Coordinate Y, Coordinate X\n");
+	fprintf(spc, "Simulation step, Vehicle name, Speed X, Position Y\n");*/
+	
+	//initialize srand with the same seed as sumo
+	srand(get_seed()); // The seeds are used for creating different random value
+
+	int n_init[5] = {0, 0, 0, 0, 0 };
+
+	int col_car = 3;
+	double desired_gap = 66.0;
+
+	double x_incr = desired_gap, y_incr = 1.3, vx_incr = 0.01;
+	double x_val = x_incr, y_val = y_incr, vx_val = vx_incr;
+
+	int virtual_lanes = 4; //initially be 3
+	double width = 10.2;
+
+	char veh_name[40]; //
+
+	//route_id and type_id should be defined in the scenario we are running
+	char route_id[40] = "route0";
+	char type_id[40] = "lane_free_car_";
+	NumericalID v_id;
+
+	for (int j = 0; j <= col_car; j++)
+	{
+		sprintf(type_id, "lane_free_car_%d", j);
+		
+		for (int i = 0; i < n_init[j]; i++) {
+
+			sprintf(veh_name, "%s_plugin_%d", type_id, (i + 1));
+			printf("try insert!\n");
+			
+			v_id = insert_new_vehicle(veh_name, route_id, type_id, x_val, y_val, vx_val, 0, 0);
+			printf("%s inserted\n", veh_name);
+
+			y_val = y_val + y_incr;
+			if (i % virtual_lanes == (virtual_lanes - 1)) {
+				x_val += x_incr;
+
+				if (vx_val < 35) {
+					vx_val += vx_incr;
+				}
+				y_val = y_incr;
+			}
+		}
+	}
+
+	printf("Initializiation over!!!\n");
+}
+
+void simulation_step() {
+	
+	double  length, width, TS = get_time_step_length();
+	
+	int t = get_current_time_step();
+	
+	//if (t == 7202) { system("pause"); }
+
+	NumericalID* myedges = get_all_edges();  //returns an array with all the edges
+	NumericalID n_myedges = get_all_edges_size(); //returns the size of all the edges sums the number of edges retrieved above
+
+	printf("List of all edges:\n");
+	for (NumericalID i = 0; i < n_myedges; i++) {
+		printf("Edge %lld: %lld\n", i, myedges[i]);
+	}
+
+	// Print the total number of edges
+	printf("Total number of edges: %lld\n", n_myedges);
+
+	NumericalID* ids_in_edge;
+	NumericalID n_edge_ids;
+	NumericalID ego_distance;
+	char* veh_name;
+	char* ego_veh_name;
+	NumericalID front_ego_veh_id;
+	char* back_veh_name;
+	NumericalID ego_vehicle_id = 0;
+	double target_gap = 66.0;
+
+	NumericalID* neighbor_size;
+	NumericalID* front_neighbors;
+	NumericalID* front_ego_neighbor;
+	NumericalID* back_neighbors;
+	size_t front_neighbors_size; //store here the number of front neighbors for each vehicle
+	size_t back_neighbors_size; //store here the number of front neighbors for each vehicle
+	size_t front_ego_size;
+	double front_distance = 100;
+	double front_ego_distance = 66;
+	double back_distance = 100;//get vehicles up to 200 meters ahead
+	int cross_edge = 0;  //1: get neighbors that are beyond the current road segment, 0: get only vehicles within the same road edge
+
+	for (int i = 0; i < n_myedges; i++) {
+
+		n_edge_ids = get_all_ids_in_edge_size(myedges[i]); //returns the numbera of all ids in a given edge, based on the edge's id
+		
+		length = get_edge_length(myedges[i]);
+		printf("edge_length for edge %d: %I64d\n", i, length);
+		width = get_edge_width(myedges[i]);
+		printf("edge_width for edge %d: %I64d\n", i, width);
+		ids_in_edge = get_all_ids_in_edge(myedges[i]);
+		printf("ids in edge %d: %I64d\n", i, ids_in_edge);
+
+		double fx{};
+		double fy{};
+
+		if (n_edge_ids > 0)
+		{
+
+			if (all_records == NULL)
+			{
+				all_records = new Record[2000];
+				for (int g = 0; g < 2000; g++)
+				{
+					all_records[g].count = 1;
+					all_records[g].up = 0.0;
+					all_records[g].first = false;
+				}
+			}
+
+			if (flow_records == NULL)
+			{
+				flow_records = new WidthRecord[2000];
+				for (int f = 0; f < 2000; f++)
+				{
+					flow_records[f].last = 0.0;
+					flow_records[f].first = false;
+				}
+			}
+
+			
+
+			Node** power = new Node * [n_edge_ids];
+
+			for (int kk = 0; kk < n_edge_ids; kk++)
+			{
+				power[kk] = new Node[n_edge_ids];
+			}
+
+			for (int j = 0; j < n_edge_ids; j++)
+			{
+				printf("Vehicle IDs in the Current Edge ID %d: %lld\n", j, ids_in_edge[j]);
+				char* id = get_veh_type_name(ids_in_edge[j]);
+				veh_name = get_vehicle_name(ids_in_edge[j]);
+
+
+				//printf("Vehicle Type: %s\n", id);
+				//printf("Vehicle Name: %s\n", veh_name);
+
+				double errorVd, errorVy, errorAccX, errorAccY;
+				
+				int vd;
+				double vx, vy;
+				double ax_desired{ 0 }, ay_desired{ 0 }, fx_repulsive{ 0 }, fy_repulsive{ 0 }, fx_nudging{ 0 }, fy_nudging{ 0 };
+				double bound_force{ 0 };
+				double cur_speed_y = get_speed_y(ids_in_edge[j]);
+				double cur_speed_x = get_speed_x(ids_in_edge[j]);
+
+				if (!CIRCULAR_MOVEMENT)
+				{
+					front_neighbors = get_all_neighbor_ids_front(ids_in_edge[j], front_distance, cross_edge, &front_neighbors_size);
+					back_neighbors = get_all_neighbor_ids_back(ids_in_edge[j], back_distance, cross_edge, &back_neighbors_size);
+				}
+				else
+				{
+					neighbor_size = ids_in_edge;
+					//neighbors_size = -1;
+				}
+
+				if (front_neighbors == 0 && back_neighbors == 0)
+				{
+					//j += 1;
+					continue;
+				}
+
+
+
+				for (int h = 0, u = j - 1; h < back_neighbors_size; h++, u--)
+				{	
+
+					double fxi_nudging = -1 * power[u][h].x;
+					double fyi_nudging = -1 * power[u][h].y;
+
+					fx_nudging += fxi_nudging;
+					fy_nudging += fyi_nudging;
+				}
+
+
+				
+				//NumericalID* ego_front_neighbors = get_all_neighbor_ids_front(0, front_distance, 1, &front_neighbors_size);
+
+				/*printf("Front Neighbors of the Ego Vehicle (ID: %lld):\n", ego_vehicle_id);
+				for (int i = 0; i < front_neighbors_size; i++) {
+					printf("Neighbor of ego %d: %lld\n", i, ego_front_neighbors[i]);
+				}*/
+
+
+
+				/* NumericalID front_neighbor_id = ego_front_neighbors[j];
+				double current_distance_to_front_vehicle = get_relative_distance_x(0, front_neighbor_id);
+				printf("Distance of ego to Front Neighbor %d: %lf meters\n", i, current_distance_to_front_vehicle);
+
+				if (i == 0 || current_distance_to_front_vehicle < target_gap)
+				{
+					// Calculate the acceleration needed to maintain the desired gap
+					double acceleration = 1.0; /* Calculate the acceleration needed based on the current distance and desired gap ;
+
+					// Apply the calculated acceleration to the ego vehicle
+					apply_acceleration(ego_vehicle_id, acceleration, 0);
+				}*/
+				
+
+
+				for (int f = 0; f < front_neighbors_size; f++)
+				{
+					double fi_repulsive{ 0 }, fxi_repulsive{ 0 }, fyi_repulsive{ 0 };
+					double a_ego{}, b_ego{};
+					//double wx1, wx2, wy;
+
+					//wx1 = 0.5; wx2 = 0.5; wy = 0.3;
+
+					//maintain_gap(ids_in_edge[f], front_neighbors[f], target_gap);
+    				calculation_ab(a_ego, b_ego, ids_in_edge[j], front_neighbors[f], wx1, wx2, wy);
+					calculation_forces(fi_repulsive, ids_in_edge[j], front_neighbors[f], n, p, q, a_ego, b_ego);//Repulsive force
+					determin_forces_repulsive(fxi_repulsive, fyi_repulsive, fi_repulsive, ids_in_edge[j], front_neighbors[f]);
+
+					//printf("a_ego: %lf, b_ego: %lf\n", a_ego, b_ego);
+
+					power[j][f].x = fxi_repulsive;	
+					power[j][f].y = fyi_repulsive;  
+
+					fx_repulsive += fxi_repulsive;
+					fy_repulsive += fyi_repulsive;
+				}
+
+				vd = get_desired_speed(ids_in_edge[j]);
+				vx = get_speed_x(ids_in_edge[j]);
+				vy = get_speed_y(ids_in_edge[j]);
+
+				target_speed_forces(ax_desired, ay_desired, ids_in_edge[j], vd);
+				
+				double ordnungskraft = verkehrsordnungskraft(ids_in_edge[j]);
+				
+				fx = ax_desired + NUDGING_INDEX * fx_nudging + REPULSIVE_INDEX * fx_repulsive;
+				fy = ay_desired + NUDGING_INDEX * fy_nudging + REPULSIVE_INDEX * fy_repulsive + ordnungskraft;
+
+				double fy_control{ 0 };
+				double fy_control_upper = upper_boundary_forces(fy, ids_in_edge[j]);
+				double fy_control_lower = lower_boundary_forces(fy, ids_in_edge[j]);
+
+				double mid = (UPPER + LOWER) * 0.5;
+				double pos_x = get_position_x(ids_in_edge[j]);
+				double pos_y = get_position_y(ids_in_edge[j]);
+
+				if (pos_y >= mid) {
+					fy_control = fy_control_upper;
+				}
+				else {
+					fy_control = fy_control_lower;
+				}
+
+				apply_acceleration(ids_in_edge[j], fx, fy_control);
+
+				if (pos_x < all_records[ids_in_edge[j]].up)
+				{
+					++(all_records[ids_in_edge[j]].count);
+				}
+
+				all_records[ids_in_edge[j]].up = pos_x;
+				double global_counter = all_records[ids_in_edge[j]].count * 1000.0;
+
+				if (all_records[ids_in_edge[j]].first && global_counter + pos_x > 100)
+				{
+					all_records[ids_in_edge[j]].count -= 1;
+					global_counter = all_records[ids_in_edge[j]].count * 1000.0;
+				}
+				all_records[ids_in_edge[j]].first = false;
+
+				flow_records[ids_in_edge[j]].first = false;
+
+				double veh_width = get_veh_width(ids_in_edge[j]);
+
+				//printf("id: %I64d\n", ids_in_edge[j]);
+
+
+				//if (t >= 0 && t <= 6000)
+				//{
+				//	fprintf(acc, "%d, %I64d, %lf, %lf\n", t, ids_in_edge[j], fx, fy_control);
+				//	fprintf(vol, "%d, %I64d, %d, %lf, %lf\n", t, ids_in_edge[j], vd, cur_speed_x, cur_speed_y);
+				//	fprintf(trj, "%d, %I64d, %lf, %lf\n", t, ids_in_edge[j], global_counter + pos_x, pos_y);
+
+				//	if (pos_x > Location_detector && flow_records[ids_in_edge[j]].last < Location_detector)
+				//	{
+				//		total_width += veh_width;
+				//		fprintf(wth, "%d, %I64d, %f, %lf\n", t, ids_in_edge[j], veh_width, total_width);
+				//		if (pos_y > 5.2) 
+				//		{
+				//			fprintf(spc, "%d, %I64d, %lf, %lf\n", t, ids_in_edge[j], vx, pos_y);
+				//		}
+				//	}
+				//}
+
+				flow_records[ids_in_edge[j]].last = pos_x;
+
+			}
+
+			for (int kk = 0; kk < n_edge_ids; kk++)
+			{
+				delete[]power[kk];
+			}
+			delete[]power;
+		}
+	}
+}
+
+void simulation_finalize() {
+	//fclose(acc);
+	/*if (all_records != NULL)
+	{
+		delete[] all_records;
+	}*/
+}
+
+void event_vehicle_enter(NumericalID veh_id) {
+
+	//set_desired_speed(veh_id, rand()%(MAX_DESIRED_SPEED - MIN_DESIRED_SPEED + 1) + MIN_DESIRED_SPEED);
+
+	//char* vname1 = get_vehicle_name(veh_id);
+	//printf("Vehicle %s entered with speed %f.\n",vname1,get_speed_x(veh_id));
+	//make the vehicles emulate a ring road scenario
+	//set_desired_speed(veh_id, vd);
+
+	set_desired_speed(veh_id, (double)(rand() % (int)((double)MAX_DESIRED_SPEED - (double)MIN_DESIRED_SPEED + 1) + (double)MIN_DESIRED_SPEED));
+	set_circular_movement(veh_id, false);
+
+	//double y_init = get_position_y(veh_id);
+	//double road_width = get_edge_width( get_edge_of_vehicle(veh_id) );
+	//double v_width = get_veh_width(veh_id);
+	//double v_desired = (double)MIN_DESIRED_SPEED + ((y_init - (v_width / 2)) / (road_width - (v_width))) * ((double)MAX_DESIRED_SPEED - (double)MIN_DESIRED_SPEED);
+	//set_desired_speed(veh_id, v_desired);
+}
+
+void event_vehicle_exit(NumericalID veh_id) {
+
+	//char* vname1 = get_vehicle_name(veh_id);
+
+	//printf("Vehicle %s existed at time %.2f, at pos:%f.\n",vname1, get_current_time_step()*get_time_step_length(), get_position_x(veh_id));
+
+	int num = 0;
+	add(&num);
+
+}
+
+void event_vehicles_collide(NumericalID veh_id1, NumericalID veh_id2) {
+
+	char vname1[40];
+	sprintf(vname1,"%s",get_vehicle_name(veh_id1));
+	char* vname2 = get_vehicle_name(veh_id2);
+	printf("Collision between %s and %s at timestep: %d, and time: %.1f.\n",vname1, vname2, get_current_time_step(), get_current_time_step()*get_time_step_length());
+	
+	double t = get_current_time_step();
+
+	//if (t >= 14400 && t <= 19200) { system("pause"); } //3600-4800 
+	//system("pause");
+}
+
+void event_vehicle_out_of_bounds(NumericalID veh_id) {
+	//char* vname1 = get_vehicle_name(veh_id);
+	//printf("Vehicle %s is out of bounds at time %.2f, at pos:%f,%f.\n", vname1, get_current_time_step() * get_time_step_length(), get_position_x(veh_id),get_position_y(veh_id));
+}
